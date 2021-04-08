e8080a795a6d0c57e36773b3c2ef2a3eab8f4afc
Jason Rivera <jar10045@nyu.edu>
2021-04-07 21:23:38 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -50,7 +50,12 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         # Fill in start
 
         # Fetch the ICMP header from the IP packet
-
+        icmpHeader = recPacket[20:28]
+        icmpType, code, mychecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+        if icmpType != 8 and packetID == ID:
+        bytesInDouble = struct.calcsize("d")
+        timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+        return timeReceived - timeSent
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
