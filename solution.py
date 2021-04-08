91e42876ddda603cb48d92b042de0662ca52038c
Jason Rivera <jar10045@nyu.edu>
2021-04-07 21:40:00 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -111,13 +111,17 @@ def ping(host, timeout=1):
     print("Pinging " + dest + " using Python:")
     print("")
     # Calculate vars values and return them
+
+
     #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
-    for i in range(0,4):
+    loop = 0
+    while loop < 10:
+    #for i in range(0,4):
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)  # one second
-
+        loop += 1
     return vars
 
 if __name__ == '__main__':
